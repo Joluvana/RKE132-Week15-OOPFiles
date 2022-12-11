1f6ea3d7dd8aa69d7487f01d3e6fb5af6437b70f
joluvana <martenhenrik@gmail.com>
2022-12-11 12:22:39 +0200

    week15 OOPFiles

---
+++ Week15OOPFiles/Program.cs
@@ -0,0 +1,57 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDAtaFromArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2], tempArray[1]);
+    myMovies.Add(newMovie);
+}
+
+foreach(Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Title} was released in {movie.Year}.Rating: {movie.Rating} ");
+}
+
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\data\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+//static void ReadDAtaFromArray(string[] someArray)
+//{
+    //foreach(string line in someArray)
+   // {
+       // Console.WriteLine(line);
+   // }
+//}
+
+class Movie
+{
+    string title;
+    string year;
+    string rating;
+
+    public string Title
+    {
+        get { return title; }
+    }
+    public string Year
+    {
+        get { return year; }
+    }
+    public string Rating
+    {
+        get { return rating; }
+    }
+    public Movie(string _title, string _year, string _rating)
+    {
+        title = _title;
+        year = _year;
+        rating = _rating;
+    }
+}
